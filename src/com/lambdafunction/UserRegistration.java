2306d90c73c043121002330d15502e3d5ed9e9a0
Girish11k <68158466+Girish11k@users.noreply.github.com>
2021-07-11 18:02:49 +0530

    commit MobileValidate push

---
+++ src/com/lambdafunction/UserRegistration.java
@@ -0,0 +1,46 @@
+/**
+ * Purpose : User Registration
+ *           User will input mobileValidate
+ *           Validate the user inputs using regex Lambda Function.
+ *           If matches, return valid else not valid
+ */
+
+package com.lambdafunction;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+    @FunctionalInterface
+    interface User{
+        void mobileValidate();
+    }
+
+    public class UserRegistration {
+
+        public static void main(String[] args ){
+            //Lambda -> operator uses
+
+            User user = () -> {
+
+                Scanner sc = new Scanner(System.in);
+
+                String regex = "^(91){1}[0-9]{9}$";
+
+                System.out.println("Enter your Mobile number");
+                String email = sc.next();
+
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(email);
+
+                if(matcher.matches()){
+                    System.out.println("Given mobile number is valid");
+                }else
+                {
+                    System.out.println("Given mobile number. is not valid");
+                }
+            };
+            user.mobileValidate();
+
+        }
+    }
